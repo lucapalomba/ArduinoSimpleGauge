ad35be116c5ccc74453ad6189b64fde8b099a8b7
lpalomba <luca.palomba.associati@gmail.com>
2016-09-13 14:28:20 +0200

    first description ad Arduino Code.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ArduinoSimpleGauge
+
+This is an example for using Arduino with the ServoMotor.
+
+I've used this simple code to convert a sequential 3 Byte expressed  in degrees (000-Max[150 for my servo]);
---
+++ gauge/gauge.ino
@@ -0,0 +1,38 @@
+#include <Servo.h>
+Servo myservo;
+char servodata[3];
+
+void setup() {
+
+  //setting for serials, and data
+  Serial.begin(115200);
+  servodata[0] =  servodata[1] =  servodata[2] = 0;
+  //setting for servo engine
+  myservo.attach(5);
+  myservo.write(0);
+
+}
+
+void loop() {
+
+  digitalWrite(enablePin, LOW);
+
+  while (Serial.available() > 3) {
+    //FIRST 3 BYTE IS SERVO DATA
+    servodata[0] = Serial.read();
+    servodata[1] = Serial.read();
+    servodata[2] = Serial.read();
+    Serial.print(servodata[0]);
+    Serial.print(servodata[1]);
+    Serial.println(servodata[2]);
+
+    //write on servo
+    Serial.print(atoi(servodata)); Serial.println("Servo degree");
+    myservo.write(atoi(servodata));
+
+    servodata[0] =  servodata[1] =  servodata[2] = 0;
+
+  }
+
+
+}
